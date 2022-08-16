38cce53863a9e5656eeae59ccf13ee8ffb52bed3
Jiexi-Ash <lance.jiexi@gmail.com>
2022-08-16 14:41:55 +0200

    added favicon

--- components/UI/MainLayout.js
+++ components/UI/MainLayout.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Head from "next/head";
 import Image from "next/image";
-import Navbar from "components/Navbar";
 import Footer from "components/Footer/Footer";
 
 function MainLayout({ children }) {
@@ -10,6 +9,7 @@ function MainLayout({ children }) {
       <Head>
         <title>Manage</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/images/favicon-32x32.png" />
       </Head>
 
       <main className="relative max-w-8xl container mx-auto overflow-hidden">
